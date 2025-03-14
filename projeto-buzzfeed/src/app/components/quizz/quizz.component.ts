c8d3de56c4a7f2ff94cdadee918d2d6febe21393
Izabela Gaspari <belaeive@gmail.com>
2025-03-14 09:17:07 -0300

    Salvando alterações

---
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent implements OnInit{
+  title:string = ""
+
+  questions:any
+  questionsSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+  questionIndex:number =0
+  questionMaxIndex:number =0
+
+  finished:boolean = false
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+}
---
+++ projeto-buzzfeed/src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { QuizzComponent } from "../../components/quizz/quizz.component";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  imports: [QuizzComponent]
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+}
